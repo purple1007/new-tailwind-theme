61ef280f08acf1903c828a4c186dd19023644bbb
purple1007 <purple1007@gmail.com>
2021-01-03 21:34:21 +0800

    Add primary color on tailwind config

--- tailwind.js
+++ tailwind.js
@@ -1,43 +1,51 @@
 module.exports = {
-    future: {
-        removeDeprecatedGapUtilities: true,
-        purgeLayersByDefault: true,
-    },
-    purge: [
-        './*.php',
-        './templates/**/*.php',
-        './build/js/**/*.js',
-    ],
-    theme: {
-        container: {
-            padding: '1.5rem',
+  future: {
+      removeDeprecatedGapUtilities: true,
+      purgeLayersByDefault: true,
+  },
+  purge: [
+      './*.php',
+      './templates/**/*.php',
+      './build/js/**/*.js',
+  ],
+  theme: {
+      container: {
+        padding: '1.5rem',
+      },
+      extend: {
+        colors: {
+          primary: '#F37960',
+          primaryBg: '#F3F2F0',
+          brown: {
+            100: '#EFEBE7',
+            200: '#E8DFD5',
+            300: '#D9CBBA',
+            400: '#C3AC92',
+            500: '#B19472',
+            600: '#70593E',
+            700: '#4B3B29',
+            800: '#1E1811'
+          }
         },
-        extend: {
-            colors: {
-                link: {
-                    default: '#3182ce',
-                    hover: '#63b3ed',
-                }
-            },
-            fontSize: {
-                xxs: '0.675rem',
-            },
-            lineHeight: {
-                tighter: '1.125',
-            },
-        }
-    },
-    variants: {
-        textColor: ['responsive', 'hover', 'focus', 'visited'],
-    },
-    plugins: [
-        ({addUtilities}) => {
-            const utils = {
-                '.translate-x-half': {
-                    transform: 'translateX(50%)',
-                },
-            };
-            addUtilities(utils, ['responsive'])
-        }
-    ]
+        fontSize: {
+          xxs: '0.675rem',
+        },
+        lineHeight: {
+          tighter: '1.125',
+        },
+      }
+  },
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'visited'],
+  },
+  plugins: [
+    ({addUtilities}) => {
+      const utils = {
+        '.translate-x-half': {
+            transform: 'translateX(50%)',
+        },
+      };
+      addUtilities(utils, ['responsive'])
+    }
+  ]
 };
