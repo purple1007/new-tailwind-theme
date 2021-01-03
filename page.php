0e7844db9d1f0c6716f6590a2c37cabd84a0508a
purple1007 <purple1007@gmail.com>
2021-01-03 16:14:12 +0800

    Add pages

---
+++ page.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package MyBlogTheme
+*/
+
+get_header();
+
+/* Start the Loop */
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'templates/partials/content-page' );
+
+	// If comments are open or there is at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) {
+		comments_template();
+	}
+endwhile; // End of the loop.
+
+get_footer();
