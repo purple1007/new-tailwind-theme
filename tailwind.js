dd726eb77db1c3d8163b22f0a024f6cfb2a6e940
purple1007 <purple1007@gmail.com>
2021-01-10 18:51:12 +0800

    Add primary colors on tailwind config

--- tailwind.js
+++ tailwind.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   future: {
       removeDeprecatedGapUtilities: true,
@@ -14,8 +16,16 @@ module.exports = {
       },
       extend: {
         colors: {
-          primary: '#F37960',
           primaryBg: '#F3F2F0',
+          primary: {
+            base: '#F37960',
+            100: '#FCD9D2',
+            200: '#FAC6BB',
+            300: '#F69F8D',
+            400: '#F58C77',
+            600: '#F05B3D',
+            700: '#EE3E1A'
+          },
           brown: {
             100: '#EFEBE7',
             200: '#E8DFD5',
@@ -30,6 +40,13 @@ module.exports = {
         fontSize: {
           xxs: '0.675rem',
         },
+        fontFamily: {
+          sans: ['system-ui', 'apple-system', 'Roboto', '"Noto Sans TC"', 'sans-serif'],
+          serif: ['"Playfair Display"', 'Lora', '"Noto Serif TC"', 'serif'],
+          mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', '"Courier New"', 'monospace'],
+          body: ['Lora', '"Noto Serif TC"', '"serif"'],
+          title: ['"Playfair Display"', '"Noto Serif TC"', 'serif']
+        },
         lineHeight: {
           tighter: '1.125',
         },
@@ -37,15 +54,29 @@ module.exports = {
   },
   variants: {
     textColor: ['responsive', 'hover', 'focus', 'visited'],
+    customPlugin: ['responsive', 'hover', 'focus', 'visited'],
   },
   plugins: [
-    ({addUtilities}) => {
-      const utils = {
-        '.translate-x-half': {
-            transform: 'translateX(50%)',
+    plugin(function({ addComponents, theme }) {
+      const buttons = {
+        '.btn': {
+          padding: `${theme('spacing.2')} ${theme('spacing.4')}`,
+          borderRadius: theme('borderRadius.none'),
+          fontWeight: theme('fontWeight.400'),
+          fontSize: theme('fontSize.base'),
+          transitionProperty: 'all',
+          transitionDuration: '100ms'
         },
-      };
-      addUtilities(utils, ['responsive'])
-    }
+        '.btn-primary': {
+          backgroundColor: theme('colors.primary.base'),
+          color: theme('colors.primaryBg')
+        },
+        '.btn-secondary': {
+          backgroundColor: theme('colors.brown.100'),
+          color: theme('colors.primary.base')
+        }
+      }
+      addComponents(buttons, ['responsive', 'hover', 'focus', 'visited']) 
+    })
   ]
 };
