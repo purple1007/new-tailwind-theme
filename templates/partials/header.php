068990ecc323d78899c680cc264aebc36346b223
purple1007 <purple1007@gmail.com>
2021-01-10 18:51:54 +0800

    Update Header layout and style

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -1,30 +1,22 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- *
- * @package MyBlogTheme
- */
-
-?>
-<!doctype html>
-<html lang="en" class="no-js">
-<head>
-	<meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= wp_get_document_title() ?></title>
-    <script>document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/, 'js');</script>
-	<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?> >
-<?php wp_body_open(); ?>
-<div id="page" class="Site bg-primaryBg">
-	<header id="masthead" class="SiteHeader mb-10">
-		<nav id="site-navigation" class="main-navigation">
-		</nav><!-- #site-navigation -->
+	<header id="masthead" class="header">
+		<nav id="site-navigation" class="header__navigation">
+      <h1 class="header__navigation__site-name">
+        <a href="/">
+          <?php bloginfo( 'name' ); ?>
+        </a>
+      </h1>
+      <?php if ( has_nav_menu( 'primary' ) ) : ?>
+        <?php
+          wp_nav_menu(
+            array(
+              'theme_location'  => 'primary',
+              'menu_class'      => 'header__navigation__links',
+              'container_class' => 'header__navigation__Primary',
+              'items_wrap'      => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
+              'fallback_cb'     => false,
+            )
+          );
+        ?>
+      <?php endif; ?>
+    </nav><!-- #site-navigation -->
   </header>
