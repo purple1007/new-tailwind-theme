479b62fe2c8fadc59112eac1653f8c91149360d5
purple1007 <purple1007@gmail.com>
2021-02-03 00:31:18 +0800

    Remove unuse js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,3 @@
-// you can import modules from the theme lib or even from
-// NPM packages if they support it…
-import ExampleComponent1 from "./components/ExampleComponent1";
-
-// you can also require modules if they support it…
-const ExampleModule2 = require('./components/example-2');
-
-// Some convenient tools to get you started…
-import ReplaceObfuscatedEmailAddresses from "./components/ReplaceObfuscatedEmailAddresses";
-import AnimateOnPageLinks from "./components/AnimateOnPageLinks";
-
 import Navbar from './components/navbar';
 import getPageTitle from './utils/getPageTitle'
 
@@ -16,8 +5,4 @@ import getPageTitle from './utils/getPageTitle'
 jQuery(function ($) {
     Navbar.init();
     getPageTitle.init();
-    // ExampleComponent1.init();
-    // ExampleModule2.init();
-    // ReplaceObfuscatedEmailAddresses.init();
-    // AnimateOnPageLinks.init();
 });
