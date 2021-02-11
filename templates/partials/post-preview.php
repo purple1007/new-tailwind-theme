772b98b18b77f2443dc927d1b86aab0a0f3f87f4
purple1007 <purple1007@gmail.com>
2021-02-11 16:57:15 +0800

    Update post-preview layout

--- templates/partials/post-preview.php
+++ templates/partials/post-preview.php
@@ -1,22 +1,27 @@
 <div class="post_preview">
   <?php if ( has_post_thumbnail()) : ?>
     <div class="post_preview__thumbnail">
+      <a 
+          href="<?php the_permalink() ?>"
+          title="<?php the_title_attribute(); ?>"
+        >
       <?php the_post_thumbnail(); ?>
+      </a>
     </div>
   <?php endif; ?>
   <div class="post_preview__content">
-    <small class="post_preview__time">
+    <small class="post_preview__detail">
       <?php the_date() ?>・<?php $category = get_the_category(); 
         echo $category[0]->cat_name; ?>
     </small>
-    <a 
-      href="<?php the_permalink() ?>"
-      title="<?php the_title_attribute(); ?>"
-    >
-      <h2 class="post_preview__title">
-        <?php the_title(); ?>
-      </h2>
-    </a>
+    <h2 class="post_preview__title">
+      <a 
+        href="<?php the_permalink() ?>"
+        title="<?php the_title_attribute(); ?>"
+      >
+      <?php the_title(); ?>
+      </a>
+    </h2>
     <?php the_excerpt();?>
     <div>
       <a href="<?php get_the_permalink(); ?>" class="post_preview__link" rel="nofollow">Read more</a>
