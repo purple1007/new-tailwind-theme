871e0ada2942b5c15bf1afc61291ab284e379746
purple1007 <purple1007@gmail.com>
2021-02-11 16:58:06 +0800

    Update post style(#1)

--- templates/partials/content/post.php
+++ templates/partials/content/post.php
@@ -1,6 +1,39 @@
-<article class="custom_post">
-  <h1><?php the_title(); ?></h1>
-  <div>
-    <?php the_content(); ?>
+<section class="post">
+  <article class="post__container">
+    <h1 class="post__title">
+      <?php the_title(); ?>
+    </h1>
+    <small class="post__detail">
+      <?php the_date() ?> ・
+      <?php $category = get_the_category(); 
+        echo $category[0]->cat_name; ?>
+    </small>
+    <?php if ( has_post_thumbnail()) : ?>
+      <div class="post__thumbnail">
+        <?php the_post_thumbnail(); ?>
+      </div>
+    <?php endif; ?>
+    <div class="post__content">
+      <?php the_content(); ?>
+    </div>
+  </article>
+
+  <section class="post___footer">
+    <div>
+      <span>上一篇文章</span>
+      <h3>
+        <?php previous_post_link('%link', '%title'); ?>
+      </h3>
+    </div>
+    <div>
+      <span>下一篇文章</span>
+      <h3>
+        <?php next_post_link('%link', '%title'); ?>
+      </h3>
+    </div>
+  </section>
+  <div class="post__back_btn">
+    <a class="wp-block-button__link" href="/blog" >回到部落格</a>
   </div>
-</article>
+
+</section>
