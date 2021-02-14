ef6130339bc4cb7bba95bc9058de1bfa6202c343
purple1007 <purple1007@gmail.com>
2021-02-14 16:58:14 +0800

    Fix typo

--- templates/partials/content/home.php
+++ templates/partials/content/home.php
@@ -10,7 +10,7 @@
         <a href="https://dribbble.com/debby" target="_blank" rel="nofollow noopener sponsored">
           設計作品
         </a>
-      。我將會在這個部落格分享設計、生活的心得，如果想要了解更多 UI 相關的知識，可以關注我和朋友一起共筆的部落格： 
+      。我將會在這個部落格分享設計、生活的心得，如果想要了解更多 UI、網頁設計相關的文章，可以關注我和朋友一起共筆的部落格： 
         <a href="http://apodesign.tw/" target="_blank" rel="noopener nofollow noopener sponsored">
           一塊設計 | apo design
         </a>  
