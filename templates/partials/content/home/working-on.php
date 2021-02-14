16b0865d0f3ae8f0c3e04f5c3edaa3c91c22a757
purple1007 <purple1007@gmail.com>
2021-02-14 14:32:39 +0800

    Add Working on section

---
+++ templates/partials/content/home/working-on.php
@@ -0,0 +1,70 @@
+<section class="home__section">
+  <h3>What I’m working on ＿＿</h3>
+  <h2>我目前正在進行＿＿</h2>
+
+  <div class="home__columns">
+    <div class="home__column">
+      <h3>UI & 網頁設計</h3>
+      <p>
+        我目前仍在從事網頁及 UI 設計的工作，我喜歡設計、喜歡透過色塊、視覺讓網站的內容能被使用者注意、使用。你可以再我的 
+        <a 
+          href="https://dribbble.com/debby" 
+          target="_blank" rel="nofollow noopener sponsored"
+        >
+          Dribbble
+        </a> 
+        或是 
+        <a 
+          href="https://www.instagram.com/debby.design/" target="_blank" rel="nofollow noopener sponsored"
+        >
+          Instagram
+        </a> 上查閱我的設計作品。
+      </p>
+    </div>
+
+    <div class="home__column">
+      <h3>文章創作與紀錄</h3>
+      <p>
+        2020 年我與身旁的設計師一起建立一個共筆部落格：
+        <a 
+          href="http://apodesign.tw/" 
+          target="_blank" 
+          rel="nofollow noopener sponsored"
+        >
+          一塊設計 | apo design
+        </a> 
+        ，而我也是其中一位作者。我們將透過這個部落格分享我們的設計想法、經歷。同時梳理我們的設計脈絡。
+      </p>
+    </div>
+
+    <div class="home__column">
+      <h3>體驗一個合作者</h3>
+      <p>
+        透過工作和共筆部落格、生活上各種境遇，很幸運能夠參與各式大小專案，在這些專案當中我也擔任過主導、幫手，各種職位。我真的真的很感謝這些專案裡我遇到的所有人，因為在團隊裡我學習到很多很多；我體認到核心理念是如此重要的影響著我與夥伴們。<br/>
+        我將會在這個部落格分享我的工作心得。
+      </p>
+    </div>
+
+    <div class="home__column">
+      <h3>網頁開發</h3>
+      <p>
+        我喜歡 HTML / CSS ，是這些文字建構出畫面。透過 CSS 美化了網頁上死板板的畫面。經歷過長期與 JavaScript 的拉扯和撞牆後，近期我也慢慢能理解 JS 強大之處；是 JS 影響、解決網頁上效能、開發等各式問題。很高興我還在學習他的路上。 <br />
+        我將會分享我的程式學習的心得在
+        <a 
+          href="/category/learn-programming" 
+          target="_self" rel="nofollow noopener sponsored"
+        >
+          這裏
+        </a>
+        。或是可以在 
+        <a 
+          href="https://github.com/purple1007"  
+          target="_blank" rel="noopener"
+        >
+          GitHub
+        </a> 
+        查閱我近期的專站。
+      </p>
+    </div>
+  </div>
+</section>
