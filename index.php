b6aeac159758ed38d3c8fdafa1508553edb99cd2
Debby <purple1007@gmail.com>
2021-02-21 11:26:22 +0800

    Fix Archive header title (#21)

--- index.php
+++ index.php
@@ -2,20 +2,8 @@
 
 <!-- Blog page -->
 <main class="custom_layout index">
-  <h1 class="page-title">
-      <?php wp_title('');?>
-    </h1>
-  <div class="custom_container">
-    <?php if (have_posts()) : 
-          while (have_posts()) : 
-          the_post(); 
-          get_template_part( 'templates/partials/post-preview' );
-      ?>
-    <?php endwhile; endif; ?>
-  
-    <!-- 分頁 -->
-    <?php get_template_part( 'templates/partials/posts-pagination' );?>
-  </div>
+  <?php get_template_part( 'templates/partials/archive/header' );?>
+  <?php get_template_part( 'templates/pages/archive' );?>
   <? get_sidebar('sidebar', $args); ?>
 </main>
 
