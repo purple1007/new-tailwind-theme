4df5960d30d921f92a4746375bb7879e65423902
purple1007 <purple1007@gmail.com>
2021-03-17 22:45:02 +0800

    Update home banner

--- templates/pages/home.php
+++ templates/pages/home.php
@@ -6,23 +6,31 @@
 
     <p class="home__description">
       Hi 我是 Debby ，我目前是位網頁設計師 / UI Designer ，從事設計的工作已經有五年的經歷了，做過平面設計、印刷設計，最後待在網頁設計、UI 設計。至今還是覺得透過程式碼刻出畫面、使用者透過 App 或網站服務完成許多事感到很神奇。<br />
-      你可以在這邊看到我的
+      你可以在
         <a href="https://dribbble.com/debby" target="_blank" rel="nofollow noopener sponsored"
-           data-event-category='HomeBanner' 
-           data-event-action='Click'
-           data-event-label='Text - 設計作品'
+           data-event-category="HomeBanner" 
+           data-event-action="Click"
+           data-event-label="Text - 設計作品"
         >
-          設計作品
-        </a>
-      。我將會在這個部落格分享設計、生活的心得，如果想要了解更多 UI、網頁設計相關的文章，可以關注我和朋友一起共筆的部落格： 
+          這邊
+        </a>看到我的設計作品。我將會在這個部落格分享設計、生活的心得，如果想要了解更多 UI、網頁設計相關的文章，可以關注我和朋友一起共筆的部落格： 
         <a href="http://apodesign.tw/" target="_blank" rel="noopener nofollow noopener sponsored"
            data-event-category="HomeBanner"
            data-event-action="Click"
            data-event-label="Text - apo design"
         >
           一塊設計 | apo design
-        </a>  
+        </a>。
     </p>
+    <div>
+      <a class="button__link" href="/blog" 
+        data-event-category="HomeBanner" 
+        data-event-action="Click"
+        data-event-label="Blog"
+      >
+        閱讀我的文章
+      </a>
+    </div>
   </section>
 
   <?php get_template_part( 'templates/partials/home/working-on' ); ?>
