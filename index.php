1297e6484dbf4e63c2f9d6317d0cf078435c075a
Debby <purple1007@gmail.com>
2022-04-04 15:25:52 +0800

    test-sidebar-remove-args

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <main class="custom_layout index">
   <?php get_template_part( 'templates/partials/archive/header' );?>
   <?php get_template_part( 'templates/pages/archive' );?>
-  <?php get_sidebar( 'sidebar', $args );?>
+  <?php get_sidebar( 'sidebar' );?>
 </main>
 
 <?php get_footer();?>
