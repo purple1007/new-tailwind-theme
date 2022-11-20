eae09ab6d1afac246d94236dc2204a3b0e5c824d
Debby <purple1007@gmail.com>
2022-11-20 13:10:41 +0800

    Feature/show projects (#85)
    * Remove working on
    
    * Add recent projects
    
    * Update home page
    
    * Update style
    
    * Update home.php
    
    Update project button
    
    * Update works page style

--- templates/partials/home/recent-projects.php
+++ templates/partials/home/recent-projects.php
@@ -1,24 +1,26 @@
 <section class="recent_posts home__section">
   <h2>
-    <a href="/blog"
-       data-event-category="HomeRecentPosts" 
-       data-event-action="Click"
-       data-event-label="Articles"
+    <a href="/category/projects"
+      data-event-category="HomeRecentProjects" 
+        data-event-action="Click"
+        data-event-label="Projects"
     >
-      Articles</a>
+      Projects
+    </a>
   </h2>
   
   <div class="recent_posts__container">
     <?php
     $recent_posts = wp_get_recent_posts(array(
         'numberposts' => 4, 
-        'post_status' => 'publish'
+        'post_status' => 'publish',
+        'category' => '29, 27'
     ));
     foreach( $recent_posts as $post_item ) : ?>
     <div>
       <figure   class="recent_posts__thumbnail">
         <a href="<?php echo get_permalink($post_item['ID']) ?>"
-            data-event-category="HomeRecentPosts" 
+            data-event-category="HomeRecentProjects" 
             data-event-action="Click"
             data-event-label="<?php echo $post_item['post_title'] ?>/Thumbnail"
         >
@@ -28,26 +30,14 @@
       
       <h3 class="recent_posts__title">
         <a href="<?php echo get_permalink($post_item['ID']) ?>"
-          data-event-category="HomeRecentPosts" 
+          data-event-category="HomeRecentProjects" 
           data-event-action="Click"
           data-event-label="<?php echo $post_item['post_title'] ?>/Title"
         >
           <?php echo $post_item['post_title'] ?>
         </a>
       </h3>
-      <small><?php echo date('n/j/Y', strtotime($post_item['post_date'])); ?>・<?php $category = get_the_category($post_item['ID']); 
-        echo $category[0]->cat_name; ?>
-      </small>
     </div>
     <?php endforeach; ?>
-  </div>
-  <div class="recent_posts__goBlog">
-    <a class="button__link" href="/blog" 
-       data-event-category="HomeRecentPosts" 
-       data-event-action="Click"
-       data-event-label="閱讀更多文章"
-    >
-      閱讀更多文章
-    </a>
   </div>
 </section>
--- templates/partials/home/recent-works.php
+++ templates/partials/home/recent-works.php
@@ -1,15 +1,15 @@
 <section class="home__section recent_works">
-    <h3>Works</h3>
-    <h2>近期作品＿＿</h2>
-    <?php if ( function_exists( 'rl_gallery' ) ) { rl_gallery( ['id' => '409'] ); }
-    ?>
-    <div class="recent_works__goWorks">
-      <a class="button__link" href="/works" 
+    <h2>
+      <a href="/works"
         data-event-category="HomeRecentWorks" 
         data-event-action="Click"
-        data-event-label="更多作品"
+        data-event-label="Works"
       >
-        更多作品
+        Works
       </a>
+    </h2>
+    <div>
+      <?php if ( function_exists( 'rl_gallery' ) ) { rl_gallery( ['id' => '409'] ); }
+      ?>
     </div>
   </section>
