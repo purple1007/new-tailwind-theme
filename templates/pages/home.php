be4d209a6abd8b66a5a4a2c87b8bad309daed398
Debby <purple1007@gmail.com>
2022-12-11 15:52:23 +0800

    Init widget on home page

--- templates/pages/home.php
+++ templates/pages/home.php
@@ -1,36 +1,7 @@
 <article class="custom_home">
   <section class="home__section">
-    <h1>Hi! I’m Debby.<br />
-      I’m a product designer.
-    </h1>
-
-    <p class="home__description">
-      Hi 我是 Debby ，我目前是位 product designer ,從事設計的工作已經有五年的經歷了，做過平面設計、品牌設計，最後待在網站設計、UI 設計。至今還是覺得透過程式碼刻出畫面、使用者透過 App 或網站服務完成許多事感到很神奇。<br />
-      你可以在
-        <a href="/works" target="_self" rel="nofollow noopener sponsored"
-           data-event-category="HomeBanner" 
-           data-event-action="Click"
-           data-event-label="Text - 設計作品"
-        >
-          這邊
-        </a>看到我的設計作品。我將會在這個部落格分享 UI 設計、生活、工作的心得，如果想要了解更多 UI、網站設計相關的文章，歡迎關注我和朋友一起共筆的部落格： 
-        <a href="http://apodesign.tw/" target="_blank" rel="noopener nofollow noopener sponsored"
-           data-event-category="HomeBanner"
-           data-event-action="Click"
-           data-event-label="Text - apo design"
-        >
-          一塊設計 | apo design
-        </a>。
-    </p>
-    <div>
-      <a class="button__link" href="/category/projects" 
-        data-event-category="HomeBanner" 
-        data-event-action="Click"
-        data-event-label="Projects"
-      >
-        Projects
-      </a>
-    </div>
+    <?php dynamic_sidebar( 'home_title_widget' ); ?>
+    <?php dynamic_sidebar( 'home_description_widget' ); ?>
   </section>
   
   <?php get_template_part( 'templates/partials/home/recent-projects' ); ?>
