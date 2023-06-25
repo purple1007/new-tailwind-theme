24abe9a82acddb563ecd16d2f9c202caa9fbf208
Debby <purple1007@gmail.com>
2023-06-25 15:36:30 +0800

    Update widget and font style

--- templates/partials/post-preview.php
+++ templates/partials/post-preview.php
@@ -24,22 +24,12 @@
       <?php the_title(); ?>
       </a>
     </h2>
+    <div class="post_preview__excerpt">
+      <?php the_excerpt();?>
+    </div>
     <small class="post_preview__detail">
       <?php the_date() ?>・<?php $category = get_the_category(); 
         echo $category[0]->cat_name; ?>
     </small>
-    <div class="post_preview__excerpt">
-      <?php the_excerpt();?>
-    </div>
-    <div>
-      <a href="<?php the_permalink() ?>" 
-         class="post_preview__link" rel="nofollow"
-         data-event-category="ArchivePost" 
-         data-event-action="Click"
-         data-event-label="<?php the_title(); ?>/Read more"
-      >
-        Read more
-      </a>
-    </div>
   </div>
 </div>
